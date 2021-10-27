ea5379af7495a9141acd4637c452ee08530eeb4a
Yannis <yannis.barba.dev@gmail.com>
2021-10-27 16:07:42 +0200

    created a data.json and add a route to getdata which allowed to download data from the API edamam => the purpose is to save data in our server in order to allowed to save favorites recipes

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
 
 const app = express();
 
@@ -10,6 +12,25 @@ const items = [
 app.get('/myroute', (req, res) => {
   console.log('handling /myroute');
   res.send(items);
+});
+
+app.get('/getdata', (req, res) => {
+  axios
+    .get(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=lasagne&app_id=f3601de5&app_key=960c7d96572cfedbc3eb6bffbfaf24c9`
+    )
+    .then((response) => response.data)
+    .then((data) => {
+      console.log(data.hits);
+      fs.writeFile('data.json', JSON.parse(data.hits), 'utf-8', (err) => {
+        if (err) {
+          console.log('An error occured while writing JSON Object to File.');
+          return console.log(err);
+        }
+
+        console.log('JSON file has been saved.');
+      });
+    });
 });
 
 app.listen(5000, () => console.log('server listening on port 5000'));
