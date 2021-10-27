b7be36cb8417acf461ac1d9a72f82f53123fdcff
Yannis <yannis.barba.dev@gmail.com>
2021-10-27 22:22:33 +0200

    add two id to the favorites array in order to do test in the front branch

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const favorites = [];
+const favorites = [
+  '#recipe_0f6199b0c6a6283e57cf42056aaf6f1f',
+  '#recipe_7af45ab44d7a01aa241239c9cbac8884',
+];
 
 app.get('/favorites', (req, res) => {
   console.log('handling /favorites');
