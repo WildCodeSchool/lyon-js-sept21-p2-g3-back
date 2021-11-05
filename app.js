417bbe5385782a1369e2fe85be9f85ebb696bc52
Dr Wouse <e.gon66@outlook.fr>
2021-11-05 15:43:49 +0100

    add recipe to the planning on user's post

--- app.js
+++ app.js
@@ -15,33 +15,13 @@ let favorites = [];
 let planning = [
   {
     date: 'Mercredi 03 2021',
-    time: 'midday',
+    lunch: true, 
+    diner: false, 
     title: 'lasagne',
     img: 'https://assets.afcdn.com/recipe/20200408/109520_w1024h1024c1cx1866cy2800.jpg',
     id: 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f',
   },
-  {
-    date: 'Mercredi 03 2021',
-    time: 'night',
-    title: 'lasagne',
-    img: 'https://assets.afcdn.com/recipe/20200408/109520_w1024h1024c1cx1866cy2800.jpg',
-    id: 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f',
-  },
-  {
-    date: 'Jeudi 04 2021',
-    time: 'midday',
-    title: 'lasagne',
-    img: 'https://assets.afcdn.com/recipe/20200408/109520_w1024h1024c1cx1866cy2800.jpg',
-    id: 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f',
-  },
-  {
-    date: 'Jeudi 04 2021',
-    time: 'night',
-    title: 'lasagne',
-    img: 'https://assets.afcdn.com/recipe/20200408/109520_w1024h1024c1cx1866cy2800.jpg',
-    id: 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f',
-  },
-];; 
+];
 
 const cors = require('cors');
 
@@ -64,7 +44,7 @@ app.use('/', router);
 
 app.use(express.json());
 
-// FAVORITES 
+// FAVORITES
 app.get('/favorites', (req, res) => {
   console.log('handling /favorites');
   res.send(favorites);
@@ -80,9 +60,21 @@ app.post('/favorites/:id', (req, res) => {
 
 // PLANNING
 
-app.get("/planning", (req, res) =>{
-  console.log('On planning')
+app.get('/planning', (req, res) => {
+  console.log('On planning');
   res.send(planning);
-})
+});
+
+app.post('/addtoplanning/', (req, res) => {
+  planning.push({
+    id: req.body.id,
+    date: req.body.date, 
+    lunch: req.body.lunch,
+    diner: req.body.diner, 
+    img: req.body.image, 
+    title: req.body.title
+  })
+  console.log(planning);
+});
 
 app.listen(5000, () => console.log('server listening on port 5000'));
