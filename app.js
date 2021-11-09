d391bdfc55a8bdd2d0abe2f3bdf9b6d64114f1c7
Dr Wouse <e.gon66@outlook.fr>
2021-11-09 15:04:52 +0100

    create function addIngredients which either add an ingredients to the listIngredients either add a unity of ingredients

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // Importation
 require('dotenv').config();
 const express = require('express');
@@ -12,16 +13,59 @@ const router = express.Router();
 // 'recipe_7af45ab44d7a01aa241239c9cbac8884',
 
 let favorites = [];
+
 let planning = [
   {
     date: 'Mercredi 03 2021',
-    lunch: true, 
-    diner: false, 
+    lunch: true,
+    diner: false,
     title: 'lasagne',
     img: 'https://assets.afcdn.com/recipe/20200408/109520_w1024h1024c1cx1866cy2800.jpg',
     id: 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f',
   },
 ];
+
+let listIngredients = [  {
+  text: '7.7 oz (220 g) mozzarella cheese',
+  quantity: 7.699999809265137,
+  measure: 'ounce',
+  food: 'mozzarella cheese',
+  weight: 218.2913226552576,
+  foodCategory: 'Cheese',
+  foodId: 'food_acjhpy7bkl7a9qboztipaa2i9e4m',
+  image: 'https://www.edamam.com/food-img/92d/92d92d4a4dfe8c025cea407c9ce764c3.jpg'
+}];
+
+const addIngredients = (ingredients) => {
+  // if (listIngredients.length === 0) {
+  //   for (let i = 0; i < ingredient.length; i +=1) {
+  //     // console.log( 'ingredient[i]', ingredient[i])
+  //     listIngredients.push(ingredient[i]);
+  //   }
+  // } else {
+    for (let newIng of ingredients) {
+      console.log('newIng', newIng)
+      let isInList = false;
+      for (let oldIng of listIngredients) {
+        if (newIng.foodId === oldIng.foodId) {
+          isInList = true;
+          oldIng.quantity += newIng.quantity;
+        }
+      }
+      if (!isInList) {
+        listIngredients.push(newIng);
+      }
+    }
+  // }
+};
+
+const removeUnity = (ingredient) => {
+
+}
+
+const addUnity = (ingredient) => {
+
+}
 
 const cors = require('cors');
 
@@ -68,13 +112,15 @@ app.get('/planning', (req, res) => {
 app.post('/addtoplanning/', (req, res) => {
   planning.push({
     id: req.body.id,
-    date: req.body.date, 
+    date: req.body.date,
     lunch: req.body.lunch,
-    diner: req.body.diner, 
-    img: req.body.image, 
-    title: req.body.title
-  })
-  console.log(planning);
+    diner: req.body.diner,
+    img: req.body.image,
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+  });
+  addIngredients(req.body.ingredients);
+  console.log('listIngredients', listIngredients);
 });
 
 app.listen(5000, () => console.log('server listening on port 5000'));
