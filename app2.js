1e4546ead007516d3105ee33551cf2e14e23cce2
Yannis <yannis.barba.dev@gmail.com>
2021-11-16 12:33:48 +0100

    created post route -> still some bugs

---
+++ app2.js
@@ -0,0 +1,90 @@
+/* eslint-disable */
+// Importation
+require('dotenv').config();
+const connection = require('./db-config');
+const cors = require('cors');
+const express = require('express');
+const router = express.Router();
+
+// Connection
+
+const port = process.env.PORT || 3000;
+
+const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS.split(',');
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (origin === undefined || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+// Initialisation of the app
+const app = express();
+app.use(cors(corsOptions));
+// add function to the app
+app.use('/', router);
+
+app.use(express.json());
+
+// ROUTE
+
+// Creation des routers :
+
+const favoritesRouter = express.Router();
+app.use('/favorites', favoritesRouter);
+
+// Favorites :
+//Get Favorites
+favoritesRouter.get('/', (req, res) => {
+  connection
+    .promise()
+    .query('SELECT * FROM favorites')
+    .then(([results]) => {
+      console.log(results);
+      res.status(200).json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('error retrieving favorites');
+    });
+});
+
+// Post Favorites
+
+favoritesRouter.post('/:id', (req, res) => {
+  const id = req.params.id;
+  const isFavorite = req.body.isfavorite;
+  console.log(req.body.isfavorite);
+  if (isFavorite) {
+    connection
+      .promise()
+      .query('INSERT INTO favorites (user_id, id_recipe) VALUES (?,?)', [1, id])
+      .then(([results]) => {
+        res.status(200).send('favorites added');
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send('error when adding favorite');
+      });
+  } else {
+    connection
+      .promise()
+      .query('DELETE FROM favorites WHERE user_id = ? AND id_recipe = ?', [
+        1,
+        id,
+      ])
+      .then(([results]) => {
+        res.status(204).send('favorites deleted');
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send('error when deleting favorite');
+      });
+  }
+});
+
+app.listen(port, () => console.log(`server listening on port ${port}`));
---
+++ reset-db.sql
@@ -0,0 +1,70 @@
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `listes`;
+DROP TABLE IF EXISTS `ingredients`;
+DROP TABLE IF EXISTS `favorites`;
+DROP TABLE IF EXISTS `planning`;
+DROP TABLE IF EXISTS `suggestions`;
+
+CREATE TABLE `users` (
+	`id` INT NOT NULL AUTO_INCREMENT, 
+	`firstname` VARCHAR(255) NOT NULL, 
+	`lastname` VARCHAR(255) NOT NULL, 
+	`email` VARCHAR(255) NOT NULL, 
+	PRIMARY KEY (`id`)
+);
+
+INSERT INTO `users` (id, firstname, lastname, email) VALUES (1, 'pedro', 'genthon', 'leroilion@genthon.fr');
+
+CREATE TABLE `ingredients` (
+	`id` VARCHAR(255) NOT NULL, 
+	`name` VARCHAR(255) NOT NULL, 
+	`measure`VARCHAR(255) NOT NULL, 
+	`category` VARCHAR(255), 
+	`image` VARCHAR(1000),
+	PRIMARY KEY (`id`)	
+);
+
+INSERT INTO `ingredients` (`id`, `name`, `measure`, `category`, `image`) 
+VALUES ('food_acjhpy7bkl7a9qboztipaa2i9e4m', 'mozzarella cheese', 'ounce', 'Cheese', 'https://www.edamam.com/food-img/92d/92d92d4a4dfe8c025cea407c9ce764c3.jpg' );
+
+CREATE TABLE `listes` (
+	`date` DATE NOT NULL,
+	`user_id` INT NOT NULL, 
+	`id_ingredient` VARCHAR(255) NOT NULL, 
+	`quantity` FLOAT NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES users(`id`),
+	FOREIGN KEY (`id_ingredient`) REFERENCES ingredients(`id`),
+	PRIMARY KEY (`date`, `user_id`, `id_ingredient`)
+); 
+
+INSERT INTO `listes` (`date`, `user_id`, `id_ingredient`, `quantity`) 
+VALUES ('2021-11-16', 1, 'food_acjhpy7bkl7a9qboztipaa2i9e4m', '2');
+
+CREATE TABLE `favorites` (
+	`user_id` INT NOT NULL, 
+	`id_recipe` VARCHAR(255) NOT NULL, 
+	FOREIGN KEY (`user_id`) REFERENCES users(`id`),
+	PRIMARY KEY (`user_id`, `id_recipe`)
+);
+
+INSERT INTO `favorites` (`user_id`, `id_recipe`) VALUES ('1', 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f');
+
+CREATE TABLE `planning` (
+	`user_id` INT NOT NULL, 
+	`date` DATE NOT NULL, 
+	`time` VARCHAR(255) NOT NULL,
+	`id_recipe` VARCHAR(255) NOT NULL, 
+	FOREIGN KEY (`user_id`) REFERENCES users(`id`), 
+	PRIMARY KEY (`user_id`, `date`, `time`)	
+);
+
+INSERT INTO planning (`user_id`, `date`, `time`, `id_recipe`) VALUES (1, '2021-11-16', 'lunch', 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f');
+INSERT INTO planning (`user_id`, `date`, `time`, `id_recipe`) VALUES (1, '2021-11-16', 'dinner', 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f');
+
+CREATE TABLE `suggestions` (
+	`date` DATE NOT NULL, 
+	`id_recipe` VARCHAR(255) NOT NULL, 
+	PRIMARY KEY (`date`, `id_recipe`)
+);
+
+INSERT INTO suggestions (`date`, `id_recipe`) VALUES ('2021-11-16', 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f');
