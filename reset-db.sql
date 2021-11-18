3ec0c895713fac26d97863d3e5da128ba8832a03
Yannis <yannis.barba.dev@gmail.com>
2021-11-18 15:07:38 +0100

    delete fake data from the data base except the unique user

--- reset-db.sql
+++ reset-db.sql
@@ -24,8 +24,6 @@ CREATE TABLE `ingredients` (
 	PRIMARY KEY (`id`)	
 );
 
-INSERT INTO `ingredients` (`id`, `name`, `measure`, `category`, `image`) 
-VALUES ('food_acjhpy7bkl7a9qboztipaa2i9e4m', 'mozzarella cheese', 'ounce', 'Cheese', 'https://www.edamam.com/food-img/92d/92d92d4a4dfe8c025cea407c9ce764c3.jpg' );
 
 CREATE TABLE `listes` (
 	`date` DATE NOT NULL,
@@ -37,9 +35,6 @@ CREATE TABLE `listes` (
 	PRIMARY KEY (`date`, `user_id`, `id_ingredient`)
 ); 
 
-INSERT INTO `listes` (`date`, `user_id`, `id_ingredient`, `quantity`) 
-VALUES ('2021-11-16', 1, 'food_acjhpy7bkl7a9qboztipaa2i9e4m', '2');
-
 CREATE TABLE `favorites` (
 	`user_id` INT NOT NULL, 
 	`id_recipe` VARCHAR(255) NOT NULL, 
@@ -48,8 +43,6 @@ CREATE TABLE `favorites` (
 	FOREIGN KEY (`user_id`) REFERENCES users(`id`),
 	PRIMARY KEY (`user_id`, `id_recipe`)
 );
-
-INSERT INTO `favorites` (`user_id`, `id_recipe`, `image`, `label`) VALUES ('1', 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f', 'https://www.edamam.com/food-img/4d6/4d651eaa8a353647746290c7a9b29d84.jpg', 'mon label');
 
 CREATE TABLE `planning` (
 	`user_id` INT NOT NULL, 
@@ -62,8 +55,6 @@ CREATE TABLE `planning` (
 	FOREIGN KEY (`user_id`) REFERENCES users(`id`), 
 	PRIMARY KEY (`user_id`, `date`, `lunch`, `dinner`)	
 );
-
-INSERT INTO planning (`user_id`, `date`, `lunch`, `dinner` , `id_recipe`, `image`, `label` ) VALUES (1, '2021-11-16', true, false, 'recipe_0f6199b0c6a6283e57cf42056aaf6f1f', 'https://www.edamam.com/food-img/4d6/4d651eaa8a353647746290c7a9b29d84.jpg', 'mon label');
 
 CREATE TABLE `suggestions` (
 	`date` DATE NOT NULL, 
